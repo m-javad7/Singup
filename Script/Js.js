b7748f2788e9de93984efab4c56da323b065500f
javad7721 <m.javad7721@gmail.com>
2023-05-22 18:51:32 +0330

    add Js: add checkPassword

--- Script/Js.js
+++ Script/Js.js
@@ -1,3 +1,4 @@
+//check FirstName
 const checkFirstName = () => {
     let input_text = document.querySelector("#fname");
     let fnameEror = document.querySelector(".error_msg");
@@ -12,3 +13,54 @@ const checkFirstName = () => {
         fnameEror.textContent = "";
     }
 }
+//check Password
+const checkPassword = () => {
+    let isUpper = false;
+    let isLower = false;
+    let isNumber = false;
+
+    let passwordInput = document.getElementById("password");
+    let passwordError = document.getElementById("passwordError");
+    let password = passwordInput.value;
+
+    for (let i = 0; i < password.length; i++) {
+        const character = password.charCodeAt(i);
+
+        if (character >= 48 && character <= 57) {
+            isNumber = true;
+        }
+
+        if (character >= 65 && character <= 90) {
+            isUpper = true;
+        }
+
+        if (character >= 97 && character <= 122) {
+            isLower = true;
+        }
+    }
+    if ( password.length < 8) {
+        passwordInput.classList.add("error_msg");
+        passwordInput.style.border = "1px solid #f74040";
+        passwordError.textContent = "Password must have at least 8 digits";
+    }else if (!isLower) {
+        passwordInput.classList.add("error_msg");
+        passwordInput.style.border = "2px solid #f74040";
+        passwordError.textContent = "Password should contain at least one lowercase letter";
+    } else if (!isUpper) {
+        passwordInput.classList.add("error_msg");
+        passwordInput.style.border = "1px solid #f74040";
+        passwordError.textContent = "Password should contain at least one uppercase letter";
+    } else if (!isNumber) {
+        passwordInput.classList.add("error_msg");
+        passwordInput.style.border = "1px solid #f74040";
+        passwordError.textContent = "Password should contain at least one digit";
+    } else {
+        passwordInput.classList.remove("error_msg");
+        passwordInput.style.border = "1px solid #80d084";
+        passwordError.textContent = "";
+    }
+
+    if (isUpper && isLower && isNumber && password.length >= 8) {
+        console.log("Password is valid");
+    }
+};
