e31f633839321987788052a90b04379edd82054d
javad7721 <m.javad7721@gmail.com>
2023-05-23 20:08:13 +0330

    fix js:Change letter to 15

--- Script/Js.js
+++ Script/Js.js
@@ -2,10 +2,10 @@
 const checkFirstName = () => {
     let input_text = document.querySelector("#fname");
     let fnameEror = document.querySelector(".error_msg");
-    if (input_text.value.length <= 3) {
+    if (input_text.value.length <= 15) {
         input_text.style.border = "1px solid #f74040";
         input_text.classList.add("error_msg");
-        fnameEror.textContent = "The name must be at least 3 letters long. Please try again";
+        fnameEror.textContent = "The name must be at least 15 letters long. Please try again";
         return false;
     } else {
         input_text.classList.remove("error_msg");
